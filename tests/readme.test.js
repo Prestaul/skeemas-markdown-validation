e862f72e3fd415b792d801efaf56a9b8bc3b49dd
Matt Dunlap <matt.dunlap@welltok.com>
2015-05-07 14:24:14 -0600

    Add ref for 'foo' schema in readme tests

--- tests/readme.test.js
+++ tests/readme.test.js
@@ -5,8 +5,9 @@ skeemas.use(skeemasMarkdownValidation);
 
 describe('README.md', function() {
 	it('should have valid json', function() {
-		var validator = skeemas();
-		validator.addRef(require('./schemas/foo-new.schema'));
-		validator.validateMarkdown(__dirname + '/../README.md');
+		skeemas()
+			.addRef(require('./schemas/foo-new.schema'))
+			.addRef(require('./schemas/foo.schema'))
+			.validateMarkdown(__dirname + '/../README.md');
 	});
 });
